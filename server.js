9f974066cf59800062f2f3d2713098523c4132ea
Hossam E. Hassan Ali <hossam_7asebat@hotmail.com>
2021-03-30 09:07:25 +0200

    intial push

--- server.js
+++ server.js
@@ -2,18 +2,25 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const port=5000;
+const express = require('express');
+const cors = require('cors');
+const bodyParser=require('body-parser');
 // Start up an instance of app
-
+const app=express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
-
+const server= app.listen(port,callbackListener);
 
 // Setup Server
+function callbackListener()
+{
+    console.log('Server Started at port '+port);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-
+const baseUrl='http://api.openweathermap.org/data/2.5/weather?zip=';
+const appKey='&appid=a713fb484fccd4229d435f8cfb36e9cd';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
